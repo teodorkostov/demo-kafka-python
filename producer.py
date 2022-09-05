830fa74d38162df8abbf6f664dc3f7646ac7dc20
Teodor Kostov <kostov.teodor@gmail.com>
2022-09-05 12:24:04 +0300

    Use ns precision

--- producer.py
+++ producer.py
@@ -9,7 +9,8 @@ producer = Producer({'bootstrap.servers': 'broker'})
 rx.interval(1).pipe(
   # ops.map(lambda index: {"name": str(index), "timestamp": int(1000 * time.time())})
   # ops.map(lambda index: f'rx,tag=me index={index} {int(1000 * time.time())}')
-  ops.map(lambda index: f'rx,tag=me index={index}')
+  ops.map(lambda index: f'rx,tag=me index={index} {time.time_ns()}')
+  # ops.map(lambda index: f'rx,tag=me index={index}')
 # ).subscribe(lambda message: producer.produce(topic='quickstart', value=json.dumps(message)))
 ).subscribe(lambda message: producer.produce(topic='quickstart', value=message))
 
